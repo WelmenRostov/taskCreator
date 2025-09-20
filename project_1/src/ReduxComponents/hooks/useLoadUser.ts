860391f7c77172a2f0705afe25c11e33e0f702e4
Nemakhov Andrei <welmenrostov@gmail.com>
2025-09-20 23:35:46 +0300

    Собирается

--- project_1/src/ReduxComponents/hooks/useLoadUser.ts
+++ project_1/src/ReduxComponents/hooks/useLoadUser.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { userAccessImage } from '../features/user/userThunk';
 import type { UserType } from '../features/user/authSlice';
