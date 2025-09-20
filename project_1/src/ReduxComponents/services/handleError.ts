75bcdc7e5035264d13c0ab5d1c4ff9aaae0b9988
Nemakhov Andrei <welmenrostov@gmail.com>
2025-09-20 22:50:21 +0300

    Очистка ветки под новый MR

--- project_1/src/ReduxComponents/services/handleError.ts
+++
@@ -1,6 +0,0 @@
-import { AxiosError } from 'axios';
-
-export const handleError = (err: unknown): never => {
-  const error = err as AxiosError<{ message?: string; error?: string }>;
-  throw new Error(error.response?.data?.message || error.response?.data?.error || 'Ошибка запроса');
-};
